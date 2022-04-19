d4e2e71f254ee6845a6fa73c73c5cf4a0fa2f976
NilsonAlla <103747734+NilsonAlla@users.noreply.github.com>
2022-04-19 13:36:29 +0200

    submit

--- DB/database.php
+++ DB/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "1499";
-$dbname = "nilsi";
+$host = "ec2-52-18-116-67.eu-west-1.compute.amazonaws.com";
+$username = "pjbpyncnrdiios";
+$password = "2a52a9fe43a01c80f9497c164be1752c3bb6162b2a69778c4b85a344cbcd6e78";
+$dbname = "dfpfbuc1ifm08g";
 
 // Create connection
 $conn = mysqli_connect($host, $username, $password, $dbname);
